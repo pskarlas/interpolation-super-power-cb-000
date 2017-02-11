b873e614cfbfb2c02d8b47ee3a2da9b9579fb754
Panagiotis Skarlas <pskarl@gmail.com>
2017-02-11 16:43:26 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+def display_rainbow(arr)
+  my_arr=[];
+  arr.each do |color|
+
+      my_arr.push(color[0].upcase + ": " + color )
+
+  end
+  sentence = my_arr.join(", ")
+  puts sentence
+end
